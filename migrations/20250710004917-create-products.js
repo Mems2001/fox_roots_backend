dc733103c66e0e0f33acb3a6e8c6f6a16d6ca397
Mems2001 <mems2001code@gmail.com>
2025-07-09 19:52:50 -0500

    feature: products model added

---
+++ migrations/20250710004917-create-products.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+
+    try {
+      await queryInterface.createTable('products', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        name: {
+          type: Sequelize.STRING
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      }, {transaction});
+    } catch (error) {
+      await transaction.rollback();
+
+      throw error
+    }
+  },
+  async down(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+
+    try {
+      await queryInterface.dropTable('products', {transaction});
+
+      await transaction.commit()
+    } catch (error) {
+      await transaction.rollback();
+
+      throw error
+    }
+  }
+};
