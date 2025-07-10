e09f18e133de43d5185d614b60735eb025a55fac
Mems2001 <mems2001code@gmail.com>
2025-07-09 19:48:00 -0500

    update: typescript removed

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
-import { Application, Request, Response } from "express";
-import { Error } from "sequelize";
-const express = require('express')
-const dotenv = require('dotenv');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 //API SETTINGS
 dotenv.config();
-const app:Application = express();
+const app = express();
 const port = process.env['PORT'] || 8000
 
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.json())
 
 //  Accept json & form-urlencoded
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // ROUTES
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req, res) => {
   res.status(200).json({
     status: 'up',
     maintenance: false
@@ -28,10 +28,10 @@ const db = require("../utils/database.js");
 
 db.authenticate()
     .then(() => console.log("Database correctly authenticated"))
-    .catch((err:Error) => console.log(err))
-db.sync()
+    .catch((err) => console.log(err))
+db.sync({alter: true})
     .then(() => console.log("Database correctly sincronized"))
-    .catch((err:Error) => console.log(err))
+    .catch((err) => console.log(err))
 
 app.listen(port, () => {
         console.log(`Server on PORT: ${port}`)})
--- types/types.d.ts
+++
@@ -1,3 +0,0 @@
-export type Uuid = `${string}-${string}-${string}-${string}-${string}`
-
-export type Email = `${string}@${string}.${string}`;
